40db57d5b44a72daaf2aa817361442f06eafee96
Oussama <cornos@gmail.com>
2021-11-11 15:38:41 +0100

    add buy function

--- internal/bookstore/bookstore.go
+++ internal/bookstore/bookstore.go
@@ -7,3 +7,13 @@ type Book struct {
 	Author string
 	Copies int
 }
+
+// Buy diminush the number of copies of a book if it's available.
+func Buy(b Book) Book {
+	if b.Copies == 0 {
+		return b
+	}
+
+	b.Copies--
+	return b
+}
--- internal/bookstore/bookstore_test.go
+++ internal/bookstore/bookstore_test.go
@@ -5,12 +5,42 @@ import (
 	"testing"
 )
 
+// type testCases struct {
+// 	book bookstore.Book
+// 	got  int
+// 	want int
+// }
+
 func TestBook(t *testing.T) {
 	t.Parallel()
 
 	_ = bookstore.Book{
-		Title:  "The Witcher - Last wish",
+		Title:  "The Witcher: The Last Wish",
 		Author: "Andrzej Sapkowski",
 		Copies: 3,
 	}
+}
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+
+	// tcs := []testCases{
+	// 	{bookstore.Book{Title: "The Lord of the Rings", Author: "J. R. R. Tolkien", Copies: 3}, 3, 2},
+	// 	{bookstore.Book{Title: "Dragon Age: Asunder", Author: "David Gaider", Copies: 0}, 0, 0},
+	// }
+
+	b := bookstore.Book{
+		Title:  "The Lord of the Rings",
+		Author: "J. R. R. Tolkien",
+		Copies: 2,
+	}
+
+	want := 1
+	result := bookstore.Buy(b)
+	got := result.Copies
+
+	if want != got {
+		t.Errorf("want %d copies after buying one copy from a stock of 2, got %d", want, got)
+	}
+
 }
