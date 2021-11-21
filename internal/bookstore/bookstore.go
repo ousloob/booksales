81c2bea48ae4ac32f3e91967190a53a38b3d3ca3
Oussama <cornos@gmail.com>
2021-11-21 09:39:14 +0100

    add type catalog

--- internal/bookstore/bookstore.go
+++ internal/bookstore/bookstore.go
@@ -16,6 +16,29 @@ type Book struct {
 	DiscountPercent int
 }
 
+// Catalog is a slice of Book.
+type Catalog map[int]Book
+
+// GetAllBooks shows us the list of all the books of a specific catalog.
+func (c Catalog) GetAllBooks() []Book {
+	var books []Book
+	for _, book := range c {
+		books = append(books, book)
+	}
+
+	return books
+}
+
+// GetBook gets the book based on a given id and title.
+func (c Catalog) GetBook(ID int) (Book, error) {
+	b, ok := c[ID]
+	if !ok {
+		return Book{}, fmt.Errorf("ID %d doesn't exist", ID)
+	}
+
+	return b, nil
+}
+
 // Buy diminush the number of copies of a book if it's available.
 func Buy(b Book) (Book, error) {
 	if b.Copies == 0 {
@@ -26,25 +49,7 @@ func Buy(b Book) (Book, error) {
 	return b, nil
 }
 
-// GetAllBooks shows us the list of all the books of a specific catalog.
-func GetAllBooks(catalog map[int]Book) []Book {
-	var books []Book
-	for _, book := range catalog {
-		books = append(books, book)
-	}
-	return books
-}
-
-// GetBook gets the book based on a given id and title.
-func GetBook(catalog map[int]Book, ID int) (Book, error) {
-	b, ok := catalog[ID]
-	if !ok {
-		return Book{}, fmt.Errorf("ID %d doesn't exist", ID)
-	}
-
-	return b, nil
-}
-
+// NetPriceCents calculs the price with the discount in cents.
 func (b Book) NetPriceCents() int {
 	saving := b.PriceCents * b.DiscountPercent / 100
 	return b.PriceCents - saving
--- internal/bookstore/bookstore_test.go
+++ internal/bookstore/bookstore_test.go
@@ -2,7 +2,6 @@ package bookstore_test
 
 import (
 	"booklib/internal/bookstore"
-	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -27,12 +26,6 @@ func TestBook(t *testing.T) {
 
 func TestBuy(t *testing.T) {
 	t.Parallel()
-
-	// TODO: manage a slice of test cases.
-	// tcs := []testCases{
-	// 	{bookstore.Book{Title: "The Lord of the Rings", Author: "J. R. R. Tolkien", Copies: 3}, 3, 2},
-	// 	{bookstore.Book{Title: "Dragon Age: Asunder", Author: "David Gaider", Copies: 0}, 0, 0},
-	// }
 
 	b := bookstore.Book{
 		ID:     9,
@@ -74,7 +67,7 @@ func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
 func TestGetAllBooks(t *testing.T) {
 	t.Parallel()
 
-	catalog := map[int]bookstore.Book{
+	c := bookstore.Catalog{
 		33: {ID: 33, Title: "One Piece"},
 		34: {ID: 34, Title: "The Promises Neverland"},
 		35: {ID: 35, Title: "Kimetsu No Yaiba"},
@@ -86,10 +79,7 @@ func TestGetAllBooks(t *testing.T) {
 		{ID: 35, Title: "Kimetsu No Yaiba"},
 	}
 
-	got := bookstore.GetAllBooks(catalog)
-	sort.Slice(got, func(i, j int) bool {
-		return got[i].ID < got[j].ID
-	})
+	got := c.GetAllBooks()
 
 	if !cmp.Equal(want, got) {
 		t.Error((cmp.Diff(want, got)))
@@ -99,7 +89,7 @@ func TestGetAllBooks(t *testing.T) {
 func TestGetBook(t *testing.T) {
 	t.Parallel()
 
-	catalog := map[int]bookstore.Book{
+	c := bookstore.Catalog{
 		2: {ID: 2, Title: "The Witcher: Sword of Destiny"},
 		3: {ID: 3, Title: "The Witcher: Season of Storms"},
 	}
@@ -109,7 +99,7 @@ func TestGetBook(t *testing.T) {
 		Title: "The Witcher: Season of Storms",
 	}
 
-	got, err := bookstore.GetBook(catalog, 3)
+	got, err := c.GetBook(3)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -120,9 +110,9 @@ func TestGetBook(t *testing.T) {
 }
 
 func TestGetBookBadIDReturnsError(t *testing.T) {
-	catalog := map[int]bookstore.Book{}
+	c := bookstore.Catalog{}
 
-	_, err := bookstore.GetBook(catalog, 999)
+	_, err := c.GetBook(999)
 	if err == nil {
 		t.Fatal("want error for non-esistent ID, got nil")
 	}
