43ac84e9dcf2bff1b36ff518cc874f474909a773
Oussama <cornos@gmail.com>
2022-08-17 20:57:04 +0200

    add sugar logger

--- app/services/shop-api/main.go
+++ app/services/shop-api/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"github.com/ardanlabs/service/foundation/logger"
+	"go.uber.org/zap"
 )
+
+var build = "develop"
 
 func main() {
 	// Construct the application logger.
@@ -16,4 +21,29 @@ func main() {
 	}
 
 	defer log.Sync()
+}
+
+func run(log *zap.SugaredLogger) error {
+
+	// =========================================================================
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeOut    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:12s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:default:0.0.0.0:5000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "© 1996 2022 Oussama Moulana",
+		},
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,14 @@
 module github.com/oussamm/bookstore
 
-go 1.18
+go 1.19
 
-require github.com/ardanlabs/service v0.0.0-20220607185934-612eb640eb62
+require (
+	github.com/ardanlabs/conf/v3 v3.1.2
+	github.com/ardanlabs/service v0.0.0-20220607185934-612eb640eb62
+	go.uber.org/zap v1.21.0
+)
 
 require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
 )
