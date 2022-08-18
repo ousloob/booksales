4e2e887ddc4be465540a5ceaa1241c8872b17345
Oussama <cornos@gmail.com>
2022-08-18 17:31:06 +0200

    add entrypoint

--- app/services/shop-api/main.go
+++ app/services/shop-api/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
+	"expvar"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/ardanlabs/conf/v3"
-	"github.com/ardanlabs/service/foundation/logger"
-	"go.uber.org/zap"
 )
 
 var build = "develop"
 
 func main() {
-	// Construct the application logger.
-	log, err := logger.New("SHOP-API")
-	if err != nil {
-		fmt.Println(err)
+	log := log.New(os.Stdout, "SHOP-API: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
 		os.Exit(1)
 	}
-
-	defer log.Sync()
 }
 
-func run(log *zap.SugaredLogger) error {
+func run(log *log.Logger) error {
 
 	// =========================================================================
 	// Configuration
@@ -36,14 +37,64 @@ func run(log *zap.SugaredLogger) error {
 			IdleTimeout     time.Duration `conf:"default:12s"`
 			ShutdownTimeout time.Duration `conf:"default:20s"`
 			APIHost         string        `conf:"default:0.0.0.0:3000"`
-			DebugHost       string        `conf:default:0.0.0.0:5000"`
 		}
 	}{
 		Version: conf.Version{
 			Build: build,
-			Desc:  "© 1996 2022 Oussama Moulana",
+			Desc:  "© 2021 Oussama Moulana",
 		},
 	}
+
+	const prefix = "SHOP"
+	_, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// =========================================================================
+	// App Starting
+
+	log.Printf("starting service version %s", build)
+	defer log.Println("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Printf("startup config %s", out)
+
+	expvar.NewString("build").Set(build)
+
+	// =========================================================================
+	// Start API Service
+
+	log.Print("startup status initalizing V1 API support")
+
+	// Make a channel to listen for an interrupt or terminal signal from the OS.
+	// Use a buffered channel because the signal package require it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	// Construct a server to service the request against the mux.
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeOut,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     log,
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this
+	// error.
+
+	serveErrors := make(chan error, 1)
+
+	// Start the service listening for api requests.
+	go func() {
+		log.Printf("startup status api router started host %s", api.Addr)
+		serveErrors <- api.ListenAndServe()
+	}()
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.19
 
 require (
 	github.com/ardanlabs/conf/v3 v3.1.2
-	github.com/ardanlabs/service v0.0.0-20220607185934-612eb640eb62
 	go.uber.org/zap v1.21.0
 )
 
 require (
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 )
